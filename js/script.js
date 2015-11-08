578676ef0b18b4b988aacb1374575b262993de49
Witold Budzioch <witold.budzioch@gmail.com>
2015-11-08 15:44:54 +0100

    08.11.2015 --  wykład z amial i pp
    1. hamburger menu done
    2. need to work on mobile rows 1 row for one line ?
    3. did some basic styling

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ $('.nav-nav_list li').on('click',function(e){
     $(e.target).addClass('active');
     }
 });
+$('.hamburger').on('click', function() {
+  $(this).toggleClass('open');
+  var openclose = $(this).hasClass('open') ? 'Close' : 'Open';
+  $(this).attr("title", openclose + ' Menu');
+  $( ".mobile-nav" ).slideToggle( "fast", function() {
+    // Animation complete.
+  });
+});
+
+$(window).resize(function() {
+  if($(window).width() > 740 && $('.hamburger').hasClass('open')){
+    $('.hamburger').toggleClass('open');
+    var openclose = $('.hamburger').hasClass('open') ? 'Close' : 'Open';
+    $('.hamburger').attr("title", openclose + ' Menu');
+    $( ".mobile-nav" ).slideUp( "fast", function() {
+      // Animation complete.
+    });
+  }
+});
