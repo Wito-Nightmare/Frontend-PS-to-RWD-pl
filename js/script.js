bff3126fec478ac0b36c2ed4723a8d561a05bd61
Wito Nightmare <witooostudio@gmail.com>
2015-11-08 21:57:24 +0100

    Good JOB
    messing with mobile/tablet view

--- js/script.js
+++ js/script.js
@@ -6,22 +6,32 @@ $('.nav-nav_list li').on('click',function(e){
     $(e.target).addClass('active');
     }
 });
-$('.hamburger').on('click', function() {
-  $(this).toggleClass('open');
-  var openclose = $(this).hasClass('open') ? 'Close' : 'Open';
-  $(this).attr("title", openclose + ' Menu');
-  $( ".mobile-nav" ).slideToggle( "fast", function() {
-    // Animation complete.
-  });
+$("#header__button").click(function() {
+  $(this).toggleClass("hamburger--open");
+  $( ".mobile-nav" ).slideToggle( "fast", function() { });
+  $( ".mobile-look" ).slideUp( "fast", function() { });
+   $('.mobile-look').removeClass('look--open');
+});
+
+$(".look").click(function() {
+  $( ".mobile-look" ).slideToggle( "fast", function() { });
+  $( ".mobile-nav" ).slideUp( "fast", function() { });
+  $('.mobile-look').toggleClass('look--open');
+  $('#header__button').removeClass('hamburger--open');
+});
+
+$(".main").click(function() {
+  $( ".mobile-look" ).slideUp( "fast", function() { });
+  $( ".mobile-nav" ).slideUp( "fast", function() { });
+  $('#header__button').removeClass('hamburger--open');
+   $('.mobile-look').removeClass('look--open');
 });
 
 $(window).resize(function() {
-  if($(window).width() > 740 && $('.hamburger').hasClass('open')){
-    $('.hamburger').toggleClass('open');
-    var openclose = $('.hamburger').hasClass('open') ? 'Close' : 'Open';
-    $('.hamburger').attr("title", openclose + ' Menu');
-    $( ".mobile-nav" ).slideUp( "fast", function() {
-      // Animation complete.
-    });
+  if($(window).width() > 740 && ($('#header__button').hasClass('hamburger--open') || $('.mobile-look').hasClass('look--open'))){
+    $('#header__button').removeClass('hamburger--open');
+     $('.mobile-look').removeClass('look--open');
+    $( ".mobile-nav" ).slideUp( "fast", function() { });
+    $( ".mobile-look" ).slideUp( "fast", function() { });
   }
 });
